42e9d5609cf1ac6af5b75ba7cf4ab9d192a0abd6
Igor Sorokopud <igorztsorokopud@gmail.com>
2016-10-20 20:28:49 +0300

    add new function

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 window.onload = function () {
     /* FizzBuzz */
-    for (var i = 1; i <= 100; ++i) {
-        if (i % 15 == 0) {
-            console.log('FizzBuzz');
-        } else if (i % 5 == 0) {
-            console.log('Buzz');
-        } else if (i % 3 == 0) {
-            console.log('Fizz');
-        } else {
-            console.log(i);
+    function fizzBuzz() {
+        for (var i = 1; i <= 100; ++i) {
+            if (i % 15 == 0) {
+                console.log('FizzBuzz');
+            } else if (i % 5 == 0) {
+                console.log('Buzz');
+            } else if (i % 3 == 0) {
+                console.log('Fizz');
+            } else {
+                console.log(i);
+            }
         }
     }
+
+    //fizzBuzz();
 
     var chess = '';
     for (var j = 0; j <= 8; ++j) {
@@ -19,17 +23,80 @@ window.onload = function () {
                 chess += ' ';
             } else {
                 chess += '#';
+
             }
         }
         chess += "\n"
     }
-    console.log(chess);
+    //console.log(chess);
 
-    /*  */
-    function min (arg1, arg2) {
+    function min(arg1, arg2) {
         var result = arg1 < arg2 ? arg1 : arg2;
         return result;
     }
 
-    console.log(min(-56, 11));
+    //console.log(min(-56, 11));
+
+    function isEven(arg) {
+
+        if (arg === 0) {
+            console.log("true");
+        } else if (arg === 1) {
+            console.log("false");
+        } else {
+            isEven(arg - 2);
+        }
+    }
+
+    //console.log(isEven(10));
+
+    function countBs(stringB, symbol) {
+        var counter = 0;
+        for (var i = 0; i < stringB.length; ++i) {
+            if (stringB[i] === symbol) {
+                counter++;
+            }
+
+        }
+
+        if (counter) {
+            return counter;
+        } else {
+            return 'такой буквы нет!';
+        }
+    }
+
+    //console.log(countBs('dfdf3434', 'd'));
+
+    function range(start, finish, step) {
+        var resultArray = [];
+        step = step || 1;
+
+        if (step > 0) {
+            for (; start <= finish; start += step) {
+                resultArray.push(start);
+            }
+        } else {
+            for (; start >= finish; start += step) {
+                resultArray.push(start);
+            }
+        }
+
+        return resultArray;
+    }
+
+    //console.log(range(5, 1, -1));
+
+    function sum(array) {
+        var lengthArray = array.length;
+        var result = 0;
+
+        for (var i = 0; i < lengthArray; ++i) {
+            result = result + array[i];
+        }
+
+        return result;
+    }
+
+    //console.log(sum(range(1, 10)));
 };
